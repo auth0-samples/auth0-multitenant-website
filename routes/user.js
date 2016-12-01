751d3e5beab5453c0b665ad01c2e04f07bce0afc
Shawn Meyer <sgmeyer@gmail.com>
2016-12-01 10:39:35 -0600

    Moving current tenant check to server

--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,20 @@ router.get('/', ensureLoggedIn, function(req, res, next) {
   // The host, which may contain tenant (e.g. tenant1.yourcompany.com)
   var host= req.get('host');
   var hostParts = host.split('.');;
+  var currentTenant;
+
+  if (hostParts.length > 2) {
+    currentTenant = hostParts[0];
+
+    // removes the tenant from the host parts.
+    hostParts.shift();
+  }
 
   // The host without tenant (e.g. yourcompany.com)
-  var topLevelHost = hostParts.length > 2 ? hostParts.shift().join('.') : host;
+  var topLevelHost = hostParts.join('.')
 
-  res.render('user', { user: req.user, host: topLevelHost });
+
+  res.render('user', { user: req.user, host: topLevelHost, currentTenant: currentTenant });
 });
 
 module.exports = router;
