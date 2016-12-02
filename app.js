6c5ecb27a654c060c0f4516d907c5ab14770a214
Peter Stromquist <peter@twistedstream.com>
2016-12-01 22:16:04 -0600

    Replace hard-coded domain names with env vars

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var strategy = new Auth0Strategy({
     domain:       process.env.AUTH0_DOMAIN,
     clientID:     process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL
+    callbackURL:  `http://${process.env.ROOT_DOMAIN}:${process.env.PORT}/callback`
   }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
@@ -56,7 +56,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: {
-    domain: 'yourcompany.com'
+    domain: process.env.ROOT_DOMAIN
   }
 }));
 app.use(passport.initialize());
